bdb7afa3297467ddcc8eb6a2380353222ea33fe3
Paul Munyao <paulmunyao094@gmail.com>
2022-02-06 02:06:29 +0300

    Delete quote componet from import on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
 import { FormComponent } from './form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
