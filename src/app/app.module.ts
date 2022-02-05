dc3f2f6a8733be87054c8501d7c04a5dd4ad7a18
Paul Munyao <paulmunyao094@gmail.com>
2022-02-06 02:23:21 +0300

    Add API from  material UI to angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
