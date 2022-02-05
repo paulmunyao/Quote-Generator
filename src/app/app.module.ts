d361c7b7b3f47e880403ab24e576835e6eb3cfed
Paul Munyao <paulmunyao094@gmail.com>
2022-02-06 02:07:28 +0300

    Delete quote component from NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent,
     FormComponent
   ],
   imports: [
