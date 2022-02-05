0daf9d313aa432b8baafced451203764e4f1052d
Paul Munyao <paulmunyao094@gmail.com>
2022-02-06 01:45:29 +0300

    Set margin and padding to 0px for the whole document

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormModule } from '@angular/form';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
