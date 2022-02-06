a1bff2f6c72ac090724d08a5fdcadd62e129b044
Paul Munyao <paulmunyao094@gmail.com>
2022-02-06 12:50:47 +0300

    Add property vote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,9 @@
 export class Quote {
   private name: string;
   private description: string;
+  private vote: number;
 
-  constructor(name: string,description: string) {
+  constructor(name: string,description: string,vote:number;) {
     this.name = name;
     this.description = description;
   }
