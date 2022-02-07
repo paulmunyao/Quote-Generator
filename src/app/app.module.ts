cca1c14f6d7160d62e8e5c834c6eb85034cb5868
Paul Munyao <paulmunyao094@gmail.com>
2022-02-07 12:40:34 +0300

    Create quote details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormComponent } from './form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
+    DetailsComponent,
   ],
   imports: [
     BrowserModule,
