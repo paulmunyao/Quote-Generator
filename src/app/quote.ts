d635a0ad357903b69f6d3999d378bd21f39703f5
Paul Munyao <paulmunyao094@gmail.com>
2022-02-09 02:10:49 +0300

    Remove name and add quote in the constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,12 +1,12 @@
 export class Quote {
   public id:number = 0;
-  public name: string;
+  public quote: string;
   public author: string;
   number: number;
 
-  constructor(id:number,name: string,author: string,quote:string) {
-    this.number =0;
-    this.name = name;
+  constructor(id:number,author: string,quote:string) {
+    this.number = 0;
+    this.quote = quote;
     this.author = author;
   }
 }
