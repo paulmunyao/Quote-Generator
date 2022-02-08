3db960cd1f536ad16329060e66556b21f6dff6d0
Paul Munyao <paulmunyao094@gmail.com>
2022-02-09 02:48:33 +0300

    Creating a path for quote in quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-// import {Quote } from './quote';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
 export class QuoteComponent implements OnInit {
-
-   quotes: Quote[] = [
-    {quote:'They came an ate our food',author:'~Morocco Proverb~'},
-    {quote:'A house without books is like a house without windows',author:'~Anonymous~'},
-    {quote:'Lack is when preparation meets success',author:'~Paul~'},
+  quotes: Quote[] = [
+    { quote: 'They came an ate our food', author: '~Morocco Proverb~' },
+    {
+      quote: 'A house without books is like a house without windows',
+      author: '~Anonymous~',
+    },
+    { quote: 'Lack is when preparation meets success', author: '~Paul~' },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
