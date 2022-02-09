7b139cac9c91f2dab3384a7f178bae6e0a8a5223
Paul Munyao <paulmunyao094@gmail.com>
2022-02-09 03:06:11 +0300

    Add publisher where one can enter their name after publishing a quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,10 @@
 export class Quote {
   public quote: string;
   public author: string;
-
+  public publisher: string;
   constructor(author: string, quote: string) {
     this.quote = quote;
     this.author = author;
+    this.publisher = publisher;
   }
 }
