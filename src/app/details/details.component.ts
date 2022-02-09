b2180f1666cd1dcde62b52975e958ab48b16a672
Paul Munyao <paulmunyao094@gmail.com>
2022-02-09 03:16:21 +0300

    Import quote component to quote details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-details',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
-
+@Input() quote:Quote;
   constructor() { }
 
   ngOnInit(): void {
